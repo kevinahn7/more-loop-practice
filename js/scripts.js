57e71876bf506fc0c095c6470fd8804367137921
Kevin Ahn and Kyle Parsons <kyle@>
2018-06-05 11:04:36 -0700

    add ability to test answer without showing result

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   $(".puzzle").submit(function(event) {
     event.preventDefault();
     $(".puzzle").hide();
-    $(".fail").show();
+    $(".attempt").show();
+    $(".try").show();
     var quote = $(".quote").val();
     var array = quote.split("");
     for (var x=0; x < array.length; x++) {
@@ -11,10 +12,20 @@ $(document).ready(function() {
         $(".answer").text(array.join(""));
       }
     };
-  })
-  $(".fail").click(function() {
-    $(".puzzle").show();
-    $(".fail").hide();
+
+
+    $(".attempt").submit(function(event) {
+      event.preventDefault();
+      $(".puzzle").show();
+      $(".hide").hide();
+      $(".disable").attr("disabled", "disabled");
+      var inputtedText = $(".inputtedText").val();
+      if (inputtedText.toLowerCase() === quote.toLowerCase()) {
+        $(".result").text("You got it good job!");
+      } else {
+        $(".result").text("Sorry, but you are not worthy");
+      }
+    });
   })
 
 });
