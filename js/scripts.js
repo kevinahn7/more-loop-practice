0bf7d26999bd5e62baf431b840f312df791515a5
Kevin Ahn and Kyle Parsons <kyle@>
2018-06-05 10:37:14 -0700

    remove replaced

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,10 @@ $(document).ready(function() {
     $(".fail").show();
     var quote = $(".quote").val();
     var array = quote.split("");
-    var replaced = [];
     for (var x=0; x < array.length; x++) {
       if (array[x].toLowerCase() === "a" || quote[x] === "e" || quote[x] === "i" || quote[x] === "o" || quote[x] === "u") {
         array[x] = "-";
-        replaced = array;
-        $(".answer").text(replaced.join(""));
+        $(".answer").text(array.join(""));
       }
     };
   })
