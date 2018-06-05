e9ac4961cb240c103477d4698ee2a81de7df4afb
Kevin Ahn and Kyle Parsons <kevin@>
2018-06-05 10:31:33 -0700

    Add function to show answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-  var quote = $(".quote").text();
-  var array = quote.split("");
-  var replaced = [];
+  $(".puzzle").submit(function(event) {
+    event.preventDefault();
+    $(".puzzle").hide();
+    $(".fail").show();
+    var quote = $(".quote").val();
+    var array = quote.split("");
+    var replaced = [];
+    for (var x=0; x < array.length; x++) {
+      if (array[x].toLowerCase() === "a" || quote[x] === "e" || quote[x] === "i" || quote[x] === "o" || quote[x] === "u") {
+        array[x] = "-";
+        replaced = array;
+        $(".answer").text(replaced.join(""));
+      }
+    };
+  })
+  $(".fail").click(function() {
+    $(".puzzle").show();
+    $(".fail").hide();
+  })
 
-  for (var x=0; x < array.length; x++) {
-    if (array[x].toLowerCase() === "a" || quote[x] === "e" || quote[x] === "i" || quote[x] === "o" || quote[x] === "u") {
-      array[x] = "-";
-      replaced = array;
-      $(".quote").text(replaced.join(""));
-    }
-  };
 });
